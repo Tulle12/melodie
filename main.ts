342836570d40f2a07ba2de9314025dba71011649
Tulle12 <72298643+Tulle12@users.noreply.github.com>
2020-10-03 13:55:10 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,17 @@
+input.onButtonPressed(Button.A, function () {
+    music.playMelody("C5 A B G A F G E ", 120)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("" + (input.temperature()))
+})
+basic.setLedColor(basic.rgbw(
+100,
+100,
+100,
+0
+))
+basic.pause(5000)
+basic.turnRgbLedOff()
 basic.forever(function () {
 	
 })
